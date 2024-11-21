53b0e9a2af8006df49f4f96ba21fcaf67f9b39c7
Ilma Ismail <139671858+IlmaIsmail@users.noreply.github.com>
2024-11-21 02:19:17 +0000

    calculations and data passaga

--- day5/src/App.js
+++ day5/src/App.js
@@ -1,10 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Calculator from './components/Calculator';
 
 function App() {
+  const [theme,setTheme]=useState(true);
+  const [fsize,setFsize]=useState(10);
+  const lightTheme ={
+    backgroundColor: "white", //background-color: #282c34;
+    color: '#282c34',
+    fontSize:fsize+"px"
+  }
+  const darkTheme ={
+    backgroundColor: "#282c34",
+    color: 'white',
+    fontSize:fsize+"px"
+  }
   return (
     <div className="App">
-       
+      <header className="App-header" style={theme?lightTheme:darkTheme}>
+        <Calculator/>
+        <hr></hr>
+        <button onClick={()=>{setTheme(!theme)}}>{theme?"Go Dark":"Go Light"}</button>
+        <br/>
+        Font Size:<input type='range' 
+        name="fontchg" min='10' max='20' 
+        step='1' value={fsize} onChange={(e)=>{setFsize(e.target.value); console.log(fsize)}}/>
+      </header>
     </div>
   );
 }
