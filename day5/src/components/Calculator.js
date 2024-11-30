0f4bae05b993934c5aaafea141ed5ed6e8d637ad
Ilma Ismail <ilmaismail029@gmail.com>
2024-11-30 10:30:28 +0530

    parsetInt

--- day5/src/components/Calculator.js
+++ day5/src/components/Calculator.js
@@ -1,43 +1,52 @@
 import { useState } from "react";
 
-export default function Calculator(){
-    const [num1,setNum1]=useState(0);
-    const [num2,setNum2]=useState(0);
-    return(
-<>
-        <table>
-            <tr>
-                <td align="right">Number:</td>
-                <td align="left">
-                    <input type="number" value={num1}
-                    onChange={(e)=>{setNum1(e.target.value)}}/>
-                </td>
-            </tr>
-            <tr>
-                <td align="right">Number:</td>
-                <td align="left">
-                    <input type="number" value={num2}
-                    onChange={(e)=>{setNum2(e.target.value)}}/>
-                </td>
-            </tr>
-            <tr>
-                <td align="right">Sum:</td>
-                <td>{(num1+num2)}</td>
-            </tr>
-            <tr>
-                <td align="right">Subtraction:</td>
-                <td>{num1-num2}</td>
-            </tr>
-            <tr>
-                <td align="right">Multiplication:</td>
-                <td>{num1*num2}</td>
-            </tr>
-            <tr>
-                <td align="right">Division:</td>
-                <td>{num1/num2}</td>
-            </tr>
-        </table>
-</>
-
-    );
+export default function Calculator() {
+  const [num1, setNum1] = useState(0);
+  const [num2, setNum2] = useState(0);
+  return (
+    <>
+      <table>
+        <tr>
+          <td align="right">Number:</td>
+          <td align="left">
+            <input
+              type="number"
+              value={num1}
+              onChange={(e) => {
+                setNum1(e.target.value);
+              }}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td align="right">Number:</td>
+          <td align="left">
+            <input
+              type="number"
+              value={num2}
+              onChange={(e) => {
+                setNum2(e.target.value);
+              }}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td align="right">Sum:</td>
+          <td>{parseInt(num1) + parseInt(num2)}</td>
+        </tr>
+        <tr>
+          <td align="right">Subtraction:</td>
+          <td>{num1 - num2}</td>
+        </tr>
+        <tr>
+          <td align="right">Multiplication:</td>
+          <td>{num1 * num2}</td>
+        </tr>
+        <tr>
+          <td align="right">Division:</td>
+          <td>{num1 / num2}</td>
+        </tr>
+      </table>
+    </>
+  );
 }
